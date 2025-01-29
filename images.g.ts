c1e76ebb1066e7a06c986f2f0ed671b0df43be81
Legboy <161484363+Legboy45@users.noreply.github.com>
2025-01-29 23:26:31 +0100

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,234 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "Collectible_gear":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . f 4 4 f . . . . . . 
+. . . f f . f 4 4 f . f f . . . 
+. . . f 4 f 4 2 2 4 f 4 f . . . 
+. . . . f 2 2 2 2 2 4 f . . . . 
+. . f f 2 2 2 f f 2 2 4 f f . . 
+. f c 2 2 2 f . . f 2 2 4 4 f . 
+. f c c 2 2 f . . f 2 2 2 4 f . 
+. . f f c 2 2 f f 2 2 2 f f . . 
+. . . . f c 2 2 2 2 2 f . . . . 
+. . . f c f c 2 2 2 f c f . . . 
+. . . f f . f c c f . f f . . . 
+. . . . . . f c c f . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "Collectible_skull":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f d d d d d d f . . . . 
+. . . f d b b b b b b d f . . . 
+. . . f b b b b b b b d f . . . 
+. . . f b f f b b f f d f . . . 
+. . . f b f f b b f f d f . . . 
+. . . f c b b b b b b d f . . . 
+. . . . f c b b b b b f . . . . 
+. . . . . f c b b b f . . . . . 
+. . . . . f c c c c f . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image4":
+            case "Collectible_water":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . f f . . . . . . 
+. . . . . . . f 1 f . . . . . . 
+. . . . . . f 1 f . . . . . . . 
+. . . . . f 1 1 f . . . . . . . 
+. . . . . f 1 9 1 f . . . . . . 
+. . . . . f 1 9 9 1 f . . . . . 
+. . . . f 6 9 9 9 9 1 f . . . . 
+. . . . f 6 9 9 9 9 1 f . . . . 
+. . . . f 6 9 9 9 9 1 f . . . . 
+. . . . f 6 9 9 9 9 1 f . . . . 
+. . . . . f 6 6 6 6 f . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image5":
+            case "Collectible_cd":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . f f d d d d f f . . . . 
+. . . . f d 5 5 5 5 d f . . . . 
+. . . f 4 5 5 f f 5 5 d f . . . 
+. . . f 4 5 f . . f 5 d f . . . 
+. . . f 4 5 f . . f 5 d f . . . 
+. . . f 4 5 5 f f 5 5 d f . . . 
+. . . . f 4 5 5 5 5 4 f . . . . 
+. . . . f f 4 4 4 4 f f . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image6":
+            case "points_blue":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f 9 9 9 9 f . . . . . 
+. . . . f 8 6 6 6 6 9 f . . . . 
+. . . . f 8 6 6 6 6 9 f . . . . 
+. . . . . f 8 6 6 8 f . . . . . 
+. . . . . . f 8 8 f . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image7":
+            case "points_red":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . f f 4 4 f f . . . . . 
+. . . . . f 4 2 2 4 f . . . . . 
+. . . . f c 2 2 2 2 4 f . . . . 
+. . . . f c 2 2 2 2 4 f . . . . 
+. . . . . f c 2 2 c f . . . . . 
+. . . . . f f c c f f . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image8":
+            case "points_green":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . f 7 7 f . . . . . . 
+. . . . . f 8 6 6 7 f . . . . . 
+. . . . . f 8 6 6 7 f . . . . . 
+. . . . . f 8 6 6 7 f . . . . . 
+. . . . . f 8 6 6 7 f . . . . . 
+. . . . . . f 8 8 f . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image9":
+            case "points_orange":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f 5 5 5 5 f . . . . . 
+. . . . f 2 4 4 4 4 5 f . . . . 
+. . . . f 2 4 4 4 4 5 f . . . . 
+. . . . . f 2 2 2 2 f . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image10":
+            case "points_purple":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . f b b f . . . . . . 
+. . . . . f b a a b f . . . . . 
+. . . . f c a a a a b f . . . . 
+. . . . f c a a a a b f . . . . 
+. . . . f c a a a a b f . . . . 
+. . . . . f c c c c f . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image11":
+            case "MenuPointsDisplay":return img`
+. . f f f f . . . . . . . . . . 
+. f 9 9 9 9 f . . . . . . . . . 
+f 8 6 6 6 6 9 f . . . . . . . . 
+f 8 6 6 6 6 9 f . . . f f . . . 
+. f 8 6 6 8 f . . f f 4 4 f f . 
+. . f 8 8 f . . . f 4 2 2 4 f . 
+. . . f f . . . f c 2 2 2 2 4 f 
+. . . . . . . . f c 2 2 2 2 4 f 
+. . . . . f f . . f c 2 2 c f . 
+. . . . f b b f . f f c c f f . 
+. . . f b a a b f . . f f . . . 
+. . f c a a a a b f . . . . . . 
+. . f c a a a a b f . . . . . . 
+. . f c a a a a b f . . . . . . 
+. . . f c c c c f . . . . . . . 
+. . . . f f f f . . . . . . . . 
+`;
+            case "image1":
+            case "Collectible_tree":return img`
+. . . . . . . . . . . . . . . . 
+. . . . f f . . . . . . . . . . 
+. . . f 7 7 f . . . . . . . . . 
+. . f 7 6 6 7 f . f f f f . . . 
+. f 7 6 6 6 6 f f f d d d f f . 
+. . f 6 6 6 6 f 1 f 5 5 5 d f . 
+. f 6 6 6 6 f 1 f 5 f f 5 5 d f 
+f 8 8 6 6 f 1 1 f f . . f 5 d f 
+. f f 8 6 f 1 9 1 f . . f 5 d f 
+. . . f 8 f 1 9 9 1 f f 5 5 d f 
+. . . f f 6 9 9 9 9 1 f 5 4 f . 
+. . . . f 6 9 9 9 9 1 f 4 f f . 
+. . . . f 6 9 9 9 9 1 f f . . . 
+. . . . f 6 9 9 9 9 1 f . . . . 
+. . . . . f 6 6 6 6 f . . . . . 
+. . . . . . f f f f . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
